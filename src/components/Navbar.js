8724245065dbea0ed4b5d835cb1422de67506e8c
piyaakotharii <19bcs3544@cuchd.in>
2021-07-11 13:02:47 +0530

    add product to cart option

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as CartSVG } from '../images/cart.svg';
+import { useCart } from '../lib/cart.context';
 
 const LINKS = [{ link: '/', text: 'Home' }];
 
 const Navbar = () => {
+
+  const cart = useCart();
+  const cartItemsTotal = cart.reduce((total, item) => total + item.quantity, 0);
+  const cartPriceTotal = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
+
   return (
     <div>
       {LINKS.map(({ link, text }) => (
@@ -14,7 +24,9 @@ const Navbar = () => {
         </div>
       ))}
       <div>
+      {cartItemsTotal}
         <CartSVG width={25} />
+        {cartItemsTotal}
       </div>
     </div>
   );
